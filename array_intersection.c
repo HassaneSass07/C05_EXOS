74403baf749f8aa9575ebdc7f1da0fbe3a16e773
HassaneSass07 <sasshassane02@gmail.com>
2025-05-29 23:28:20 +0100

    yeswecan

--- array_intersection.c
+++ array_intersection.c
@@ -3,67 +3,73 @@
 int main() {
     int n1, n2, i, j, k = 0;
 
-    // Lecture de la taille du premier tableau
+    // Lire la taille du premier tableau
+    printf("Taille du premier tableau : ");
     scanf("%d", &n1);
     int tab1[n1];
+
+    // Lire les éléments du premier tableau
+    printf("Éléments du premier tableau : ");
     for (i = 0; i < n1; i++) {
         scanf("%d", &tab1[i]);
     }
 
-    // Lecture de la taille du second tableau
+    // Lire la taille du deuxième tableau
+    printf("Taille du deuxième tableau : ");
     scanf("%d", &n2);
     int tab2[n2];
+
+    // Lire les éléments du deuxième tableau
+    printf("Éléments du deuxième tableau : ");
     for (i = 0; i < n2; i++) {
         scanf("%d", &tab2[i]);
     }
 
-    // Taille maximale de l'intersection = taille du plus petit tableau
-    int taille_max;
-    if (n1 < n2) {
-        taille_max = n1;
-    } else {
-        taille_max = n2;
-    }
+    // Choisir une taille suffisante pour stocker l'intersection
+    int max_inter;
+    if (n1 < n2)
+        max_inter = n1;
+    else
+        max_inter = n2;
 
-    int inter[taille_max]; // Tableau pour stocker l'intersection sans doublons
+    int intersection[max_inter];
 
-    // Parcours du premier tableau
+    // Trouver les éléments communs
     for (i = 0; i < n1; i++) {
-        int present_dans_tab2 = 0;
+        int commun = 0;
 
-        // Vérifie si tab1[i] est présent dans tab2
+        // Chercher tab1[i] dans tab2[]
         for (j = 0; j < n2; j++) {
             if (tab1[i] == tab2[j]) {
-                present_dans_tab2 = 1;
+                commun = 1;
                 break;
             }
         }
 
-        // Si l'élément n'existe pas dans tab2, on continue
-        if (present_dans_tab2 == 0) {
+        // S'il n'existe pas dans tab2, passer au suivant
+        if (!commun)
             continue;
-        }
 
-        // Vérifie si l'élément est déjà dans inter[]
-        int deja_present = 0;
+        // Vérifier s'il est déjà dans l'intersection
+        int deja_ajoute = 0;
         for (j = 0; j < k; j++) {
-            if (inter[j] == tab1[i]) {
-                deja_present = 1;
+            if (intersection[j] == tab1[i]) {
+                deja_ajoute = 1;
                 break;
             }
         }
 
-        // Si pas encore présent, on l'ajoute
-        if (deja_present == 0) {
-            inter[k] = tab1[i];
+        // Ajouter à l'intersection s'il n'est pas déjà là
+        if (!deja_ajoute) {
+            intersection[k] = tab1[i];
             k++;
         }
     }
 
-    // Affichage de l'intersection
+    // Afficher l'intersection
     printf("Intersection :");
     for (i = 0; i < k; i++) {
-        printf(" %d", inter[i]);
+        printf(" %d", intersection[i]);
     }
     printf("\n");
 
