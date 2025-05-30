8062a40f51ec9fc892f951e837e96196f2b2c477
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 02:07:58 +0100

    bonnenuit

--- array_intersection.c
+++ array_intersection.c
@@ -2,39 +2,49 @@
 
 int main() {
     int n1, n2;
-    scanf("%d", &n1);             
+    
+    scanf("%d", &n1);
     int a[n1];
-    for (int i = 0; i < n1; i++)
-        scanf("%d", &a[i]);       
-
-    scanf("%d", &n2);             
-    int b[n2];
-    for (int i = 0; i < n2; i++)
-        scanf("%d", &b[i]);       
-
-    // Parcours du premier tableau
     for (int i = 0; i < n1; i++) {
-        int deja_affiche = 0;
-
-        // Vérifier si a[i] a déjà été traité
-        for (int k = 0; k < i; k++) {
-            if (a[i] == a[k]) {
-                deja_affiche = 1;
+        scanf("%d", &a[i]);
+    }
+    
+    scanf("%d", &n2);
+    int b[n2];
+    for (int i = 0; i < n2; i++) {
+        scanf("%d", &b[i]);
+    }
+    
+    int first_element = 1;
+    
+    for (int i = 0; i < n1; i++) {
+        // Éviter les doublons en vérifiant les éléments précédents
+        int is_duplicate = 0;
+        for (int prev = 0; prev < i; prev++) {
+            if (a[i] == a[prev]) {
+                is_duplicate = 1;
                 break;
             }
         }
-
-        if (deja_affiche) continue;
-
-        // Chercher a[i] dans le deuxième tableau
+        if (is_duplicate) continue;
+        
+        // Chercher dans le deuxième tableau
+        int found = 0;
         for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
-                printf("%d ", a[i]);  // Afficher si élément commun
+                found = 1;
                 break;
             }
         }
+        
+        // Afficher si trouvé
+        if (found) {
+            if (!first_element) printf(" ");
+            printf("%d", a[i]);
+            first_element = 0;
+        }
     }
-
+    
     printf("\n");
     return 0;
 }
