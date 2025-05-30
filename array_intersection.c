5074079138303291e92b49237659fdecfc8b66d1
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 01:39:25 +0100

    confirmation

--- array_intersection.c
+++ array_intersection.c
@@ -2,15 +2,15 @@
 
 int main() {
     int n1, n2;
-    scanf("%d", &n1);             // Lire la taille du premier tableau
+    scanf("%d", &n1);             
     int a[n1];
     for (int i = 0; i < n1; i++)
-        scanf("%d", &a[i]);       // Lire les éléments du premier tableau
+        scanf("%d", &a[i]);       
 
-    scanf("%d", &n2);             // Lire la taille du deuxième tableau
+    scanf("%d", &n2);             
     int b[n2];
     for (int i = 0; i < n2; i++)
-        scanf("%d", &b[i]);       // Lire les éléments du deuxième tableau
+        scanf("%d", &b[i]);       
 
     // Parcours du premier tableau
     for (int i = 0; i < n1; i++) {
