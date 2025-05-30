8b667b3c53cf34c79504448a234e32b64541a28e
HassaneSass07 <sasshassane02@gmail.com>
2025-05-30 01:13:06 +0100

    unefois

--- array_intersection.c
+++ array_intersection.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    int n1, n2, i, j, k = 0;
+    int n1, n2;
 
     // Lire la taille du premier tableau
     scanf("%d", &n1);
     int a[n1];
-    for (i = 0; i < n1; i++) scanf("%d", &a[i]);
+    for (int i = 0; i < n1; i++) scanf("%d", &a[i]);
 
     // Lire la taille du deuxième tableau
     scanf("%d", &n2);
     int b[n2];
-    for (i = 0; i < n2; i++) scanf("%d", &b[i]);
+    for (int i = 0; i < n2; i++) scanf("%d", &b[i]);
 
-    int inter[n1 + n2]; // Stocke les éléments communs (sans doublons)
+    // Initialisation de l'intersection
+    int inter[n1 < n2 ? n1 : n2];  // Taille maximale possible
+    int k = 0; // Compteur pour l'intersection
 
-    // Parcourir a[]
-    for (i = 0; i < n1; i++) {
-        // Chercher a[i] dans b[]
-        for (j = 0; j < n2; j++) {
+    // Pour chaque élément de a[]
+    for (int i = 0; i < n1; i++) {
+        // Vérifier s’il est dans b[]
+        for (int j = 0; j < n2; j++) {
             if (a[i] == b[j]) {
-                // Vérifier si déjà dans inter[]
-                int deja = 0;
+                // Vérifier qu'il n'est pas déjà dans inter[]
+                int existe = 0;
                 for (int m = 0; m < k; m++) {
                     if (inter[m] == a[i]) {
-                        deja = 1;
+                        existe = 1;
                         break;
                     }
                 }
-                if (deja == 0) {
+                if (existe == 0) {
                     inter[k] = a[i];
                     k++;
                 }
-                break; // On ne cherche qu'une seule fois l'élément
+                break; // Ne pas ajouter deux fois si doublon dans b[]
             }
         }
     }
 
-    // Afficher l'intersection
+    // Affichage de l'intersection
     printf("Intersection :");
-    for (i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++) {
         printf(" %d", inter[i]);
     }
     printf("\n");
